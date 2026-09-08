9293d961d1d0d1f08df455235d190e0e6e230029
benjaminquepasa <benjamintroncoso@liceovvh.cl>
2026-09-08 12:03:06 -0300

    listo

---
+++ python/flask/fundamentos/19.-primera-app-mysql/app.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from mascota import Mascota
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    mascotas = Mascota.get_all()
+    return render_template(
+        "index.html",
+        mascotas=mascotas
+    )
+
+@app.route("/mascota/<int:id>")
+def mostrar_mascota(id):
+    mascota = Mascota.get_by_id(id)
+    if mascota is None:
+        return "Mascota no encontrada", 404
+    return render_template(
+        "mascota.html",
+        mascota=mascota
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
